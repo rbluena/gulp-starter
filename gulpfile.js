2a3d00140427f6249d457846d1fc2ed9cd2c99cc
Rabii Luena <itisrabii@gmail.com>
2020-06-06 20:22:07 +0300

    chore: initial setup of the repository

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp, { series } = require('gulp');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
+const concat = require('gulp-concat');
+const del = require('del');
+
+const jsPath = 'app/assets/js/**/*.js';
+const scssPath = 'app/assets/scss/**/*.scss'; // This might be removed if framework like TailWind is used
+const imgPath = 'app/assets/images/**/*'
+
+const copyFiles = () => console.log('Copy everything at once!');
+const copyHtml = () => gulp.src('app/*.html').pipe(gulp.dest('build'));
+
+const imgTask = () => gulp.src(imgPath).pipe(imagemin()).pipe(gulp.dest('build/assets/images'));
+const scssTask = () => console.log('css')
+const jsTask = () => {
+    return src(jsPath).pipe(sourcemaps.init())
+                        .pipe(concat('all.js'))
+                        .pipe(terser())
+                        .pipe(sourcemaps.write())
+                        .dest('build/assets/js');
+}
+
+function clean() {
+    del.sync('build');
+}
+
+// These are added configurations for TypeScript
+
+function build(cb) {
+    gulp.parallel(copyHtml, copyImages);
+    cb();
+}
+
+
+// TODO:
+// Removing comments from js files
+// Implementing ESLint and Prettier
+// Implementing PostCSS
+// Implementing Minification
+// Implementing tests if necessary
+// Using TypeScript for some projects
+
+exports.default = gulp.series(clean, build);
+
