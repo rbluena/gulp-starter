c758cf808b776377774a00a9e1a208033e40ea0c
Rabii Luena <itisrabii@gmail.com>
2020-06-10 11:15:21 +0300

    build: adding minification and improving build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
 const del = require('del');
@@ -9,40 +11,62 @@ const jsPath = 'app/assets/js/**/*.js';
 const scssPath = 'app/assets/scss/**/*.scss'; // This might be removed if framework like TailWind is used
 const imgPath = 'app/assets/images/**/*';
 
-const copyFiles = () => console.log('Copy everything at once!');
-const copyHtml = () => gulp.src('app/*.html').pipe(gulp.dest('build'));
+/**
+ * Cleaning build directories
+ */
+function clean(cb) {
+  console.log('*** Deleting build folder ***');
+  del.sync('build');
+  cb();
+}
 
-const imgTask = () =>
+/**
+ * copying HTML content to build folder
+ */
+function copyHtml(cb) {
+  gulp.src('app/*.html').pipe(gulp.dest('build'));
+  cb();
+}
+
+/**
+ * Image processing
+ */
+function imgTask(cb) {
   gulp.src(imgPath).pipe(imagemin()).pipe(gulp.dest('build/assets/images'));
-const scssTask = () => console.log('css');
+  cb();
+}
 
-const jsTask = () => {
-  return src(jsPath)
+/**
+ * Compiling SCSS
+ */
+function sassTask(cb) {
+  const processors = [require('autoprefixer'), require('cssnano')];
+
+  gulp
+    .src(scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/assets/css/'));
+  cb();
+}
+
+function jsTask(cb) {
+  gulp
+    .src(jsPath)
     .pipe(sourcemaps.init())
     .pipe(concat('all.js'))
     .pipe(terser())
     .pipe(sourcemaps.write())
-    .dest('build/assets/js');
-};
-
-function clean() {
-  console.log('*** Deleting build folder ***');
-  del.sync('build');
-}
-
-// These are added configurations for TypeScript
-
-function build(cb) {
-  gulp.parallel(copyHtml, copyImages);
+    .pipe(gulp.dest('build/assets/js'));
   cb();
 }
 
 // TODO:
 // Removing comments from js files
-// Implementing ESLint and Prettier
-// Implementing PostCSS
-// Implementing Minification
-// Implementing tests if necessary
+// Implementing Minification for JavaScript
 // Using TypeScript for some projects
+// Implementing tests if necessary
 
-exports.default = gulp.series(clean, build);
+exports.build = gulp.series(clean, copyHtml, imgTask, jsTask, sassTask);
