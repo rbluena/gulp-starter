0c5027d319b79052de4ebe4f1e3e7686bc482600
Rabii Luena <itisrabii@gmail.com>
2020-06-10 08:09:10 +0300

    chore: adding eslint and prettier configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp, { series } = require('gulp');
+const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
 const terser = require('gulp-terser');
@@ -7,32 +7,35 @@ const del = require('del');
 
 const jsPath = 'app/assets/js/**/*.js';
 const scssPath = 'app/assets/scss/**/*.scss'; // This might be removed if framework like TailWind is used
-const imgPath = 'app/assets/images/**/*'
+const imgPath = 'app/assets/images/**/*';
 
 const copyFiles = () => console.log('Copy everything at once!');
 const copyHtml = () => gulp.src('app/*.html').pipe(gulp.dest('build'));
 
-const imgTask = () => gulp.src(imgPath).pipe(imagemin()).pipe(gulp.dest('build/assets/images'));
-const scssTask = () => console.log('css')
+const imgTask = () =>
+  gulp.src(imgPath).pipe(imagemin()).pipe(gulp.dest('build/assets/images'));
+const scssTask = () => console.log('css');
+
 const jsTask = () => {
-    return src(jsPath).pipe(sourcemaps.init())
-                        .pipe(concat('all.js'))
-                        .pipe(terser())
-                        .pipe(sourcemaps.write())
-                        .dest('build/assets/js');
-}
+  return src(jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write())
+    .dest('build/assets/js');
+};
 
 function clean() {
-    del.sync('build');
+  console.log('*** Deleting build folder ***');
+  del.sync('build');
 }
 
 // These are added configurations for TypeScript
 
 function build(cb) {
-    gulp.parallel(copyHtml, copyImages);
-    cb();
+  gulp.parallel(copyHtml, copyImages);
+  cb();
 }
-
 
 // TODO:
 // Removing comments from js files
@@ -43,4 +46,3 @@ function build(cb) {
 // Using TypeScript for some projects
 
 exports.default = gulp.series(clean, build);
-
